2a1b56156a124ea634ea23ccb2b62b2e9d36d59a
Badr-1 <badrmohamedashraf@gmail.com>
2022-12-31 20:25:00 +0200

    :bookmark: Version 1.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,19 @@ package minesweeper
 
 import com.github.ajalt.mordant.rendering.TextColors.*
 
-fun main() {
-    var numberOfMines: Int
-    numberOfMines@ while (true) {
-        println(cyan("How many mines do you want on the field? "))
-        numberOfMines = readln().toInt()
-        if (numberOfMines in 1..80) { // number of mines can't exceed number of cells
-            break@numberOfMines
-        } else {
-            println(red("Number of mines can't exceed number of cells"))
+object Main {
+    @JvmStatic fun main(args: Array<String>) {
+        var numberOfMines: Int
+        numberOfMines@ while (true) {
+            println(cyan("How many mines do you want on the field? "))
+            numberOfMines = readln().toInt()
+            if (numberOfMines in 1..80) { // number of mines can't exceed number of cells
+                break@numberOfMines
+            } else {
+                println(red("Number of mines can't exceed number of cells"))
+            }
         }
+        val mineField = MineField(numberOfMines)
+        mineField.play()
     }
-    val mineField = MineField(numberOfMines)
-    mineField.play()
 }
